95010e45bed3c2d821308cc3be2c1e64b38e6349
cubensquare <66881155+cubensquarets@users.noreply.github.com>
2025-06-03 14:56:44 +0530

    Create consumer.py

---
+++ scenario-3/consumer.py
@@ -0,0 +1,52 @@
+from kafka import KafkaConsumer
+import json
+import os
+
+tracker_file = 'tracker_partition.json'
+log_file = 'booking_log.json'
+
+# Reset logs
+with open(tracker_file, 'w') as file:
+    json.dump({
+        "produced": 0,
+        "topic": 0,
+        "partition": 0,
+        "consumed": 0,
+        "last_partition": 0
+    }, file)
+
+with open(log_file, 'w') as file:
+    json.dump({"0": [], "1": []}, file)
+
+consumer = KafkaConsumer(
+    'ticket_partitioned',
+    bootstrap_servers='localhost:9092',
+    auto_offset_reset='earliest',
+    group_id='rail-consumer-group-v3',
+    enable_auto_commit=True,
+    value_deserializer=lambda m: json.loads(m.decode('utf-8'))
+)
+
+def update_tracker():
+    with open(tracker_file, 'r') as file:
+        data = json.load(file)
+    data['consumed'] += 1
+    with open(tracker_file, 'w') as file:
+        json.dump(data, file)
+
+def log_booking(partition, booking):
+    try:
+        with open(log_file, 'r') as file:
+            logs = json.load(file)
+    except:
+        logs = {"0": [], "1": []}
+    logs[str(partition)].append(booking)
+    with open(log_file, 'w') as file:
+        json.dump(logs, file)
+
+print("📬 Listening to ticket_partitioned topic...")
+for message in consumer:
+    booking = message.value
+    print(f"📨 Received from Partition {message.partition}: {booking}")
+    update_tracker()
+    log_booking(message.partition, booking)
